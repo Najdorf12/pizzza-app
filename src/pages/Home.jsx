f9028166d98e832b8511a666ca1a0a3f60438d12
Agustin Morro <agustin.morro@gmail.com>
2023-11-03 18:09:22 -0300

    fix-media

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,8 +70,9 @@ const variants = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
-      ease: "easeInOut",
+      duration: 2,
+      type:"spring",
+      ease:"easeInOut"
     },
   },
 };
